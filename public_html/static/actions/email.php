1984cdccb7f687aa8a0964b6d8e31a08c42e280e
Franton Lin <franton.lin@students.olin.edu>
2015-07-06 03:28:46 -0500

    attempted unsuccessfully to implement the contact form emailing

--- public_html/static/actions/email.php
+++ public_html/static/actions/email.php
@@ -3,10 +3,17 @@ if (isset($_POST["submit"])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
+
     $message = $_POST['message'];
     $to = 'franton.lin@students.olin.edu'; 
     
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+    $body = "From $name: $email\n$message";
+    $headers = "From: contact@frantonlin.com\r\n"; 
+    $headers .= "Reply-To: $email"; 
+
+    $ccbody = "This is a copy of your message to Franton Lin.\n\n$message"
+    $ccheaders = "From: $email\r\n"; 
+    $ccheaders .= "Reply-To: $to"; 
 
     // Check if name has been entered
     if (!$_POST['name']) {
@@ -30,7 +37,8 @@ if (isset($_POST["submit"])) {
  
     // If there are no errors, send the email
     if (!$errName && !$errEmail && !$errSubject && !$errMessage) {
-        if (mail ($to, $subject, $body, $email)) {
+        if (mail($to, $subject, $body, $headers)) {
+            mail($email,"CC: $subject", $ccbody, $ccheaders);
             $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
         } else {
             $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
--- public_html/static/js/main.js
+++ public_html/static/js/main.js
@@ -1,49 +1,107 @@
 function main() {
+  (function () {
+    'use strict';
 
-(function () {
-   'use strict';
+    $("input#send").click(function() {
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var subject = $("input#subject").val();
+      var message = $("textarea#message").val();
 
-   /* ==============================================
-  	Testimonial Slider
-  	=============================================== */ 
+      alert("WHOOOOO!!");
+      if (name=="") {
+        $("input#name").css("border","1px solid red");
+        $("input#name").focus();
+        return false;
+      }
 
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
+      if (!validateEmail(email)) {
+        $("input#email").css("border","1px solid red");
+        $("input#email").focus();
+        return false;
+      }
+
+      if (subject=="") {
+        $("input#subject").css("border","1px solid red");
+        $("input#subject").focus();
+        return false;
+      }
+
+      if (message=="") {
+        $("textarea#message").css("border","1px solid red");
+        $("textarea#message").focus();
+        return false;
+      }
+    });
+
+    // $("input,textarea").jqBootstrapValidation({
+    //   preventSubmit: true,
+    //   submitError: function($form, event, errors) {
+    //     // something to have when submit produces an error
+    //   },
+    //   submitSuccess: function($form, event) {
+    //     event.preventDefault(); // prevent default submit haviour
+    //     // get values from FORM
+    //     var name = $("input#name").val();
+    //     var email = $("input#email").val();
+    //     var subject = $("input#subject").val();
+    //     var message = $("textarea#message").val();
+
+    //     $.ajax({
+    //       url: "../actions/derp.php",//TODO
+    //       type: "POST",
+    //       data: {name: name, email: email, subject: subject, message: message},
+    //       cache: false,
+    //       success: function() {
+    //         // Success message
+    //         $('#success').css("display", "block");
+   
+    //         //clear all fields
+    //         $('#contact-form').trigger("reset");
+    //       },
+    //       error: function() {
+    //         // Fail message
+    //         $('#error').css("display", "block");
+
+    //         //clear all fields
+    //         $('#contact-form').trigger("reset");
+    //       },
+    //     })
+    //   },
+    //   filter: function() {
+    //     return $(this).is(":visible");
+    //   },
+    // });
+   
+    // $("a[data-toggle=\"tab\"]").click(function(e) {
+    //   e.preventDefault();
+    //   $(this).tab("show");
+    // });
 
     /*====================================
     Show Menu on Book
     ======================================*/
     $(window).bind('scroll', function() {
-        var navHeight = $(window).height()*0.70; // Where the navigation bar changes
-        if ($(window).width() <= 494) {
-          if ($(window).scrollTop() > navHeight) {
-            $('#tf-menu .avatar').css({'opacity':0,'z-index':-1});
-          } else {
-            $('#tf-menu .avatar').css({'opacity':1,'z-index':0});
-          }
-        }
+      var navHeight = $(window).height()*0.70; // Where the navigation bar changes
+      if ($(window).width() <= 494) {
         if ($(window).scrollTop() > navHeight) {
-          $('.navbar-default').addClass('on');
-          $('#tf-menu a.navbar-brand').css({'opacity':1,'z-index':0});
+          $('#tf-menu .avatar').css({'opacity':0,'z-index':-1});
         } else {
-          $('.navbar-default').removeClass('on');
-          $('#tf-menu a.navbar-brand').css({'opacity':0,'z-index':-1});
+          $('#tf-menu .avatar').css({'opacity':1,'z-index':0});
         }
+      }
+      if ($(window).scrollTop() > navHeight) {
+        $('.navbar-default').addClass('on');
+        $('#tf-menu a.navbar-brand').css({'opacity':1,'z-index':0});
+      } else {
+        $('.navbar-default').removeClass('on');
+        $('#tf-menu a.navbar-brand').css({'opacity':0,'z-index':-1});
+      }
     });
 
     $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 80
+      target: '.navbar-default',
+      offset: 80
     })
 
   	$(document).ready(function() {
@@ -57,86 +115,45 @@ function main() {
         pager: false,
         captions: true
       });
-
-  	  $("#team").owlCarousel({
-  	 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 4],
-				        [1400, 4],
-				        [1600, 4]
-				      ],
-  	  });
-
-  	  $("#clients").owlCarousel({
-  	 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 5],
-				        [1400, 5],
-				        [1600, 5]
-				      ],
-  	  });
-
-      $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
-
-  	});
+    });
 
   	/*====================================
     Portfolio Isotope Filter
     ======================================*/
     $(window).load(function() {
-        var $container = $('#lightbox');
+      var $container = $('#lightbox');
+      $container.isotope({
+        filter: '*',
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+        }
+      });
+      $('.cat a').click(function() {
+        $('.cat .active').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
         $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
+          filter: selector,
+          animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+          }
         });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
+        return false;
+      });
+    
     });
 
-
-
-}());
-
+  }());
 
 }
+
+function validateEmail(email) {
+  var re = [a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?;
+  return re.test(email);
+}
+
 main();
