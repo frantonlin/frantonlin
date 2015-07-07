c022a415c426e4bdaa1ef0d7e11bf3434293dcd8
Franton Lin <franton.lin@students.olin.edu>
2015-07-06 23:02:16 -0500

    testing on test.php

--- public_html/static/actions/test.php
+++ public_html/static/actions/test.php
@@ -1,1 +1,1 @@
-<?php phpinfo(); ?>
+<? $headers = 'From: test@frantonlin.com'; mail('franton.lin@gmail.com', 'Test email using PHP', 'This is a test email message', $headers = 'From: test@frantonlin.com'); ?>
