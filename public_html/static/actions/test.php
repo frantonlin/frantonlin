4a995f06b736e5d1eea9acca8351e106fd6af013
Franton Lin <franton.lin@students.olin.edu>
2015-07-06 23:05:01 -0500

    testing on test.php

--- public_html/static/actions/test.php
+++ public_html/static/actions/test.php
@@ -1,1 +1,5 @@
-<? $headers = 'From: test@frantonlin.com'; mail('franton.lin@gmail.com', 'Test email using PHP', 'This is a test email message', $headers = 'From: test@frantonlin.com'); ?>
+<?php
+echo "<p>Is this supposed to show up?</p>";
+$headers = 'From: test@frantonlin.com';
+mail('franton.lin@gmail.com', 'Test email using PHP', 'This is a test email message', $headers);
+?>
