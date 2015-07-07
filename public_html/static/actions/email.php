6ee633594d39e46eac0b8a00de43c4720117ca2f
Franton Lin <franton.lin@students.olin.edu>
2015-07-07 13:38:36 -0500

    forgot semicolon

--- public_html/static/actions/email.php
+++ public_html/static/actions/email.php
@@ -21,44 +21,40 @@ if (isset($_POST["submit"])) {
     $ccheaders = "From: $email\r\n"; 
     $ccheaders .= "Reply-To: $to"; 
 
-    // $errName = False;
-    // $errEmail = False;
-    // $errSubject = False;
-    // $errMessage = False;
-
     // Check if name has been entered
     if (!$_POST['name']) {
         echo "<p>name</p>";
-        // $errName = True;
+        $errName = True;
     }
     
     // Check if email has been entered and is valid
     if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
         echo "<p>email</p>";
-        // $errEmail = True;
+        $errEmail = True;
     }
 
     // Check if subject has been entered
     if (!$_POST['subject']) {
         echo "<p>subject</p>";
-        // $errSubject = True;
+        $errSubject = True;
     }
     
     // Check if message has been entered
     if (!$_POST['message']) {
         echo "<p>message</p>";
-        // $errMessage = True;
+        $errMessage = True;
     }
 
-    echo "<p>$errName</p>"
+    echo "<p>$errName</p>";
  
-    // If there are no errors, send the email
-    // if (!$errName && !$errEmail && !$errSubject && !$errMessage) {
-    //     if (mail($to, $subject, $body, $headers)) {
-    //         mail($email,"CC: $subject", $ccbody, $ccheaders);
-    //     } else {
-    //         // error
-    //     }
-    // }
+    If there are no errors, send the email
+    if (!$errName && !$errEmail && !$errSubject && !$errMessage) {
+        echo "<p>If it gets here, it should send the email!</p>";
+        // if (mail($to, $subject, $body, $headers)) {
+        //     mail($email,"CC: $subject", $ccbody, $ccheaders);
+        // } else {
+        //     // error
+        // }
+    }
 }
 ?>
