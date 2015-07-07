1ebd03470c3227dda09c3cd01ee111b9d3ff303a
Franton Lin <franton.lin@students.olin.edu>
2015-07-07 09:36:17 -0500

    goes through to gmail! still should set up DKIM

--- public_html/static/actions/test.php
+++ public_html/static/actions/test.php
@@ -1,9 +1,9 @@
 <?php
 echo "<p>This should show up.</p>";
 $headers = 'From: test@frantonlin.com';
-if(mail('franton.lin@gmail.com', 'Test email using PHP', 'This is a test email message', $headers)) {
-    echo "<h1>IT WORKS</h1>";
-} else {
-    echo "<h1>Something's broken...</h1>";
-}
+// if(mail('franton.lin@students.olin.edu', 'Test email using PHP', 'This is a test email message', $headers)) {
+//     echo "<h1>IT WORKS</h1>";
+// } else {
+//     echo "<h1>Something's broken...</h1>";
+// }
 ?>
