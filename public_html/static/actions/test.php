1ed3a29bbf44f9e96f33a5fec6c8bfc76ea00324
Franton Lin <franton.lin@students.olin.edu>
2015-07-07 09:34:49 -0500

    testing on test.php

--- public_html/static/actions/test.php
+++ public_html/static/actions/test.php
@@ -1,7 +1,7 @@
 <?php
 echo "<p>This should show up.</p>";
 $headers = 'From: test@frantonlin.com';
-if(mail('franton.lin@students.olin.edu', 'Test email using PHP', 'This is a test email message', $headers)) {
+if(mail('franton.lin@gmail.com', 'Test email using PHP', 'This is a test email message', $headers)) {
     echo "<h1>IT WORKS</h1>";
 } else {
     echo "<h1>Something's broken...</h1>";
