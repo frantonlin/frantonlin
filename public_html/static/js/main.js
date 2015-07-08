463530f4a20cf27f38bdb484f78f79d90acad392
Franton Lin <franton.lin@students.olin.edu>
2015-07-08 00:34:50 -0500

    Added back scrolling, which was lost in SHA 3adf46f

--- public_html/static/js/main.js
+++ public_html/static/js/main.js
@@ -2,6 +2,19 @@ function main() {
   (function () {
     'use strict';
 
+    $('a.page-scroll').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 40
+            }, 900);
+          return false;
+        }
+      }
+    });
+
     $("#send").click(function(e) {
       var name = $("input#name");
       var email = $("input#email");
